87e9bd5e95bf248e577041aac65280b61bb4fdd1
yigitsahin <yiit625@gmail.com>
2023-02-07 14:38:43 +0100

    Infrastructure of Project

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.UserModel;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
+
+@RestController
+@RequestMapping("user-service")
+@RequiredArgsConstructor
+@CrossOrigin
+public class UserController {
+
+    @PostMapping("/upload-csv-file")
+    public String uploadCSVFile(@RequestParam("file") MultipartFile file) {
+
+        // validate file
+        if (file.isEmpty()) {
+            System.out.println("Paste error here!!");
+        } else {
+
+            try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+
+                CsvToBean<UserModel> csvToBean = new CsvToBeanBuilder(reader)
+                        .withType(UserModel.class)
+                        .withIgnoreLeadingWhiteSpace(true)
+                        .build();
+
+                List<UserModel> users = csvToBean.parse();
+
+                System.out.println(users.get(0).getId());
+                System.out.println(users.get(0).getName());
+                System.out.println(users.get(0).getDescription());
+                System.out.println(users.get(0).getDate());
+
+
+            } catch (Exception ex) {
+                System.out.println("Error is --> " + ex);
+            }
+        }
+
+        return "Successful message";
+    }
+}
---
+++ src/main/java/com/example/demo/dto/UserModel.java
@@ -0,0 +1,22 @@
+package com.example.demo.dto;
+
+import com.opencsv.bean.CsvBindByName;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UserModel {
+    @CsvBindByName(column = "PRIMARY_KEY")
+    private String id;
+    @CsvBindByName(column = "NAME")
+    private String name;
+    @CsvBindByName(column = "DESCRIPTION")
+    private String description;
+    @CsvBindByName(column = "UPDATED_TIMESTAMP")
+    private String date;
+}
